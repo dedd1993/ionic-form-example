cb186b4a3d5c931d858432768247111a8c6c1d9c
Daniel Delgado <dedd1993@gmail.com>
2020-01-06 11:00:13 -0500

    creating demo for manipulating input element outside template

--- src/app/home/form-example/form-example.component.ts
+++ src/app/home/form-example/form-example.component.ts
@@ -1,18 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { IonInput } from '@ionic/angular';
 
 @Component({
   selector: 'app-form-example',
   templateUrl: 'form-example.component.html',
   styleUrls: ['form-example.component.scss'],
 })
-export class FormExampleComponent {
+export class FormExampleComponent implements AfterViewInit {
+  @ViewChild('dniElemRef', { static: false }) documentNumberElm: IonInput;
+
   form: FormGroup;
 
   constructor(
     public formBuilder: FormBuilder,
   ) {
     this.initFormBuilder();
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => this.documentNumberElm.setFocus() , 1000);
   }
 
   onSubmit() {
@@ -25,7 +32,7 @@ export class FormExampleComponent {
 
   private initFormBuilder() {
     this.form = new FormGroup({
-      storeCode: new FormControl({ value: null, disabled: false }, [ ]),
+      documentNumber: new FormControl({ value: null, disabled: false }, [ ]),
       email: new FormControl({ value: null, disabled: false }, [ Validators.required, Validators.email ]),
       password: new FormControl({ value: null, disabled: false }, [ Validators.required ]),
       role: new FormControl({ value: null, disabled: false }, [ Validators.required ]),
